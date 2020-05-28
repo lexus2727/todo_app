9bb3a5fdc4f15cfd7ae515b5be8a95665d074597
lexus2727 <lexus2424@yahoo.com>
2020-05-28 18:10:15 -0400

    update

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  scope 'api/v1' do
+    resources :todos
+  end
+end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
